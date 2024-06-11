96eefe70e026198abc875e3aec614fac4955a2d7
Vlad11k <vladprokopov2509@gmail.com>
2024-06-12 00:31:38 +0300

    Add templates

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('home.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/<string:post_name>')
+def post(post_name=None):
+    return render_template('post.html')
+
+
+@app.route('/add_post')
+def add_post():
+    return render_template('add_post.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
